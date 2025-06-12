dcb5ab807b43fc1719f5e9f6f883286d2f3d4e30
PashaNiki <52444612+PashaNiki@users.noreply.github.com>
2025-06-12 18:22:43 +0400

    Update Main.java

--- src/main/java/com/nikimoonprod/Main.java
+++ src/main/java/com/nikimoonprod/Main.java
@@ -1,6 +1,6 @@
 package com.nikimoonprod;
 
-private static String msg "Hello QA.GURU!!!!! Uaaaa???!!!";
+private static String msg "Hello QA.GURU!!!!! Uaaaa??? and Pavel";
 
 public class Main {
     public static void main(String[] args) {
